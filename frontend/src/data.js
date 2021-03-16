28282e2599f9ac8d428c263dafe49a5de8fdc427
dopezera <dopezeragg@gmail.com>
2021-03-16 17:49:04 -0300

    e ai vai?

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand:'Nike',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'2',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 20,
+            brand:'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'3',
+            name:'Lacoste Camisaria',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 0,
+            brand:'Lacoste',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'4',
+            name:'Nike Pants',
+            category:'Pants',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 15,
+            brand:'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'5',
+            name:'Puma Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 65,
+            countInStock: 5,
+            brand:'Nike',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            _id:'6',
+            name:'Tommy TSHIRT',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 320,
+            countInStock: 12,
+            brand:'Nike',
+            rating: 4.8,
+            numReviews: 15,
+            description: 'high quality product',
+        },
+    ],
+};
+
+export default data;
